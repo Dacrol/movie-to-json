ec2b7c9f7ac82f8ff54ed35417472c6d6e98e3d6
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-01-19 00:12:32 +0100

    Format

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const flexjson = require('jsonflex')();
-const compression = require('compression');
-const path = require('path');
+const express = require('express')
+const flexjson = require('jsonflex')()
+const compression = require('compression')
 
 // Create express server
-const app = express();
+const app = express()
 
 // Express middleware
-app.use(compression());
-app.use(flexjson);
-app.use(express.static('www'));
+app.use(compression())
+app.use(flexjson)
+app.use(express.static('www'))
 
 app.listen(3000, () =>
   console.log('Webserver listening on port 3000')
-);
+)
