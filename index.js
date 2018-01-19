a3449ae35d32d7effc5c9963320125c89aada443
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-01-19 10:44:54 +0100

    Configure jsonflex save folder

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const flexjson = require('jsonflex')()
+const flexjson = require('jsonflex')({
+  jsonDir: '/json'
+})
 const compression = require('compression')
 
 // Create express server
