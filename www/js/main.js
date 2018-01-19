85ba5ab820826355b981bb89aa250460ce3f42b7
Jonas Ahrens <jonas.ahrens@gmail.com>
2018-01-19 17:52:05 +0100

    Only allow appending each object once

--- www/js/main.js
+++ www/js/main.js
@@ -54,11 +54,11 @@ $('#submit').click(async function (event) {
     let swedishPlot = await MovieFetcher.getPlotFromMoviezine(
       $('#swedishTitleCheckbox').is(':checked')
         ? $('#swedishTitle').val()
-        : $('#inputSearch').val()
+        : deetz.original_title
     )
     // console.log(swedishPlot)
     Object.assign(deetz, { plot_sv: swedishPlot.trim() })
-    console.log(deetz)
+    // console.log(deetz)
     renderImages([deetz.poster_path, deetz.backdrop_path])
     // @ts-ignore
     $('#json-renderer').jsonViewer(deetz, {
@@ -67,10 +67,11 @@ $('#submit').click(async function (event) {
     })
     $('#json-raw').text(JSON.stringify(deetz, null, '  '))
     $('#append').prop('disabled', false)
+    $('#append').unbind('click')
     $('#append').click(function (event) {
       $('#append').prop('disabled', true)
       event.preventDefault()
-      console.log(appended)
+      // console.log(appended)
       $('#jsonflex-save-submit').prop('disabled', false)
       appended.push(deetz)
       $('#json-renderer-save').html('')
@@ -100,7 +101,9 @@ $('#submit').click(async function (event) {
 function renderImages (paths) {
   let imghtml = ''
   paths.forEach(path => {
-    imghtml = imghtml.concat(`<img src="https://image.tmdb.org/t/p/original${path}">\n`)
+    imghtml = imghtml.concat(
+      `<img src="https://image.tmdb.org/t/p/original${path}">\n`
+    )
   })
   console.log(imghtml)
   $('#images').html(imghtml)
@@ -113,167 +116,3 @@ function titleToSlug (title) {
     .replace(/&/g, '-and-')
     .replace(/[\s\W-]+/g, '-')
 }
-
-/* {
-  "adult": false,
-  "backdrop_path": "/iB0RsWVoOXzicPi2Yy5xmTYMbho.jpg",
-  "belongs_to_collection": {
-      "id": 363369,
-      "name": "Pacific Rim Collection",
-      "poster_path": "/niTMK5k486sPBCphbvmRIZ7xBGK.jpg",
-      "backdrop_path": "/vbg4Mm8auNINPersFlYvyGLqFq0.jpg"
-  },
-  "budget": 180000000,
-  "genres": [
-      {
-          "id": 28,
-          "name": "Action"
-      },
-      {
-          "id": 878,
-          "name": "Science Fiction"
-      },
-      {
-          "id": 12,
-          "name": "Äventyr"
-      }
-  ],
-  "homepage": "",
-  "id": 68726,
-  "imdb_id": "tt1663662",
-  "original_language": "en",
-  "original_title": "Pacific Rim",
-  "overview": "När stora monster stiger upp från haven inleddes ett krig som kostar miljontals liv. För att bekämpa dessa Kaijus, skapar man jättelika robotar, sk Jaegers som styrs av två piloter. Men även dessa har problem med de hänsynslösa Kaijus. På gränsen till ett förödande nederlag vänder man sig till två osannolika hjältar. En före detta pilot och en ung tjej är plötsligt mänsklighetens sista hopp inför den stundande apokalypse.",
-  "popularity": 79.444383,
-  "poster_path": "/sCJEwEShZvruTpQ2a4yiX3Q9EyZ.jpg",
-  "production_companies": [
-      {
-          "name": "Legendary Pictures",
-          "id": 923
-      },
-      {
-          "name": "Warner Bros.",
-          "id": 6194
-      },
-      {
-          "name": "Disney Double Dare You (DDY)",
-          "id": 19750
-      },
-      {
-          "name": "Indochina Productions",
-          "id": 19751
-      }
-  ],
-  "production_countries": [
-      {
-          "iso_3166_1": "US",
-          "name": "United States of America"
-      },
-      {
-          "iso_3166_1": "CA",
-          "name": "Canada"
-      }
-  ],
-  "release_date": "2013-07-11",
-  "revenue": 407602906,
-  "runtime": 131,
-  "spoken_languages": [
-      {
-          "iso_639_1": "en",
-          "name": "English"
-      }
-  ],
-  "status": "Released",
-  "tagline": "",
-  "title": "Pacific Rim",
-  "video": false,
-  "vote_average": 6.7,
-  "vote_count": 5407,
-  "videos": {
-      "results": [
-          {
-              "id": "533ec6bfc3a36854480069bc",
-              "iso_639_1": "sv",
-              "iso_3166_1": "SE",
-              "key": "2vKz7WnU83E",
-              "name": "Trailer",
-              "site": "YouTube",
-              "size": 720,
-              "type": "Trailer"
-          }
-      ]
-  },
-  "images": {
-      "backdrops": [],
-      "posters": []
-  }
-}
- */
-/*       "vote_count": 5403,
-      "id": 68726,
-      "video": false,
-      "vote_average": 6.7,
-      "title": "Pacific Rim",
-      "popularity": 80.444383,
-      "poster_path": "/sCJEwEShZvruTpQ2a4yiX3Q9EyZ.jpg",
-      "original_language": "en",
-      "original_title": "Pacific Rim",
-      "genre_ids": [
-        28,
-        878,
-        12
-      ],
-      "backdrop_path": "/iB0RsWVoOXzicPi2Yy5xmTYMbho.jpg",
-      "adult": false,
-      "overview": "When legions of monstrous creatures, known as Kaiju, started rising from the sea, a war began that would take millions of lives and consume humanity's resources for years on end. To combat the giant Kaiju, a special type of weapon was devised: massive robots, called Jaegers, which are controlled simultaneously by two pilots whose minds are locked in a neural bridge. But even the Jaegers are proving nearly defenseless in the face of the relentless Kaiju. On the verge of defeat, the forces defending mankind have no choice but to turn to two unlikely heroes—a washed-up former pilot (Charlie Hunnam) and an untested trainee (Rinko Kikuchi)—who are teamed to drive a legendary but seemingly obsolete Jaeger from the past. Together, they stand as mankind's last hope against the mounting apocalypse.",
-      "release_date": "2013-07-11" */
-
-/* [
-  {
-    "title": "Call me by your name",
-    "productionCountries": [
-      "Italien",
-      "USA"
-    ],
-    "productionYear": 2017,
-    "length": 132,
-    "genre": "Drama",
-    "distributor": "UIP",
-    "language": "engelska",
-    "subtitles": "svenska",
-    "director": "Luca Guadagnino",
-    "actors": [
-      "Armie Hammer",
-      "Timothée Chalamet",
-      "Michael Stuhlbarg"
-    ],
-    "description": "<p>Filmen utspelas i norra Italien sommaren 1983. En ung amerikansk-italienare blir förälskad i en amerikansk student som kommer för att studera och bo hos hans familj.</p><p>Tillsammans upplever de en oförglömlig sommar - full av musik, mat och kärlek - som för evigt kommer att förändra dem.</p>",
-    "images": [
-       "call-me-poster1.jpg",
-       "call-me-poster2.jpg"
-    ],
-    "youtubeTrailers": [
-      "Z9AYPxH5NTM"
-    ],
-    "reviews": [
-      {
-        "source": "Sydsvenskan",
-        "quote": "ett drama berättat med stor ömhet",
-        "stars": 4,
-        "max": 5
-      },
-      {
-        "source": "Svenska Dagbladet",
-        "quote": "en film att förälska sig i",
-        "stars": 5,
-        "max": 5
-      },
-      {
-        "source": "DN",
-        "quote": "en het romans i åttiotalskostym",
-        "stars": 4,
-        "max": 5
-      }
-    ]
-  }
-] */
